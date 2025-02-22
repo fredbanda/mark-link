0c5854e2cfa0a78a3269d1094c3b4cafe966ae6a
Fred Banda <fredcbanda@hotmail.com>
2025-02-22 13:28:21 +0200

    set render deployment.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
